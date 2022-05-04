3378796c8177f4e1fc1a7a3c2c0d10ba1f154c08
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-04 17:31:28 -0300

    criação dos arquivos de configuração do projeto.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+	public function index() 
+	{
+		$postars = auth()->user->postars;
+
+		return response()->json([
+			'success' => true,
+			'data' => $postars
+		]);
+	}
+	public function show($id)
+	{
+		$post = auth()->user->postars()->find($id);
+
+		if (!$post) {
+			return response()->json([
+				'success' => false,
+				'message' => 'Postagem não encontrada'
+			], 400);
+		}
+		return response()->json([
+			'success' => true,
+			'data' => $post->toArray()
+		], 400);
+	}
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'titulo' => 'required',
+			'descricao' => 'required'
+		]);
+		$post = new Post();
+		$post->titulo = $request->titulo;
+		$post->descricao = $request->descricao;
+
+		if (auth()->user()->postars()->save($post)) {
+			return response()->json([
+				'success' => true,
+				'data' => $post->toArray()
+			]);
+		} else 
+		   return response()->json([
+			   'success' => false,
+			   'message' => 'Postagem não adicionada'
+		   ], 500);
+	}
+	public function update(Request $request, $id)
+	{
+		$post = auth()->user()->postars()->find($id);
+
+		if (!$post) {
+			return response()->json([
+				'success' => false,
+				'message' => 'Postagem não encontrada'
+			], 400);
+		}
+		$updated = $post->fill($request->all())->save();
+
+		if ($updated)
+		 return response()->json([
+			 'success' => true
+		 ]);
+		 else 
+		 return response()->json([
+			 'success' => false,
+			 'message' => 'Postagem não pode ser atualizada'
+		 ], 500);
+	}
+	public function destroy($id)
+	{
+		$post = auth()->user()->postars()->find($id);
+
+		if (!$post) {
+			return response()->json([
+				'success' => false,
+				'message' => 'Postagem não encontrada'
+			], 400);
+		}
+		if ($post->delete()) {
+			return response()->json([
+				'success' => true
+			]);
+		} else {
+			return response()->json([
+				'success' => false,
+				'message' => 'Postagem não pode ser deletada'
+			], 500);
+		}
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::middleware('auth:api')->group(function() {
+	Route::resource('postars', PostController::class);
 });
+Route::post('registrar', [UsuarioController::class, 'registrar']);
+Route::post('login', [UsuarioController::class, 'login']);
